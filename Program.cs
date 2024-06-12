bc04d168df24b2e016e751e3dbcae0404479aff3
Sibongile Mbewe <SMbewe@betsoftware.com>
2024-06-12 11:53:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static (int sum, int product) Calc(int a, int b)
+        {
+            return (a  + b, a * b);
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter two integers
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            // Call the Calculate method
+            var result = Calc(num1, num2);
+
+            // Display the results
+            Console.WriteLine($"Sum: {result.sum}");
+            Console.WriteLine($"Product: {result.product}");
+
+            // Wait for user input before closing
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+
+}
+
+
